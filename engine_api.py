e3c061472bfe0bf4ef30479f0c06f27f2b5a61fe
niklaslagesson <niklas.lagesson@gmail.com>
2017-05-09 22:34:55 +0200

    Added CreateApp method

--- engine_api.py
+++ engine_api.py
@@ -17,10 +17,17 @@ class EngineApi:
     def get_doc_list(self):
         msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": -1, "method": "GetDocList", "params": []})
         response = self.engine_socket.send_call(self.engine_socket, msg)
-        print json.loads(response)['result']['qDocList']
+        return json.loads(response)['result']['qDocList']
 
     # returns the os name (always windowsNT). Obsolete?
     def get_os_name(self):
         msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": -1, "method": "OSName", "params": []})
         response = self.engine_socket.send_call(self.engine_socket, msg)
-        print json.loads(response)['result']['qReturn']
+        return json.loads(response)['result']['qReturn']
+
+    # returns the app id. If desktop is used the app id is the same as the full path to qvf
+    # if it's running against Enterprise, app id will be a guid
+    def create_app(self, app_name):
+        msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": -1, "method": "CreateApp", "params": [app_name]})
+        response = self.engine_socket.send_call(self.engine_socket, msg)
+        return json.loads(response)['result']['qAppId']
