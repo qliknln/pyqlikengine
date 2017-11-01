7c38b4d5ad5c6a07d69a3e61be84f024f7b86f7f
qliknln <Talisker12>
2017-11-01 19:03:15 +0100

    Improved/bugfixed hypercube handling. Added method for creating inline table from hypercube data

--- qix_engine.py
+++ qix_engine.py
@@ -33,25 +33,47 @@ class QixEngine:
 
     def create_hypercube(self, list_of_dimensions=[], list_of_measures=[], rows_to_return=1000):
         no_of_columns = len(list_of_dimensions) + len(list_of_measures)
-        hc_inline_dim = self.Structs.nx_inline_dimension_def(list_of_dimensions)
-        hc_dim = self.Structs.nx_hypercube_dimensions(hc_inline_dim)
+        hc_dim = []
+        for d in list_of_dimensions:
+            hc_inline_dim = self.Structs.nx_inline_dimension_def([d])
+            hc_dim.append(self.Structs.nx_hypercube_dimensions(hc_inline_dim))
+        hc_mes = []
         for m in list_of_measures:
             hc_mes_sort = self.Structs.nx_sort_by()
             hc_inline_mes = self.Structs.nx_inline_measure_def(m)
-            hc_mes = self.Structs.nx_hypercube_measure(hc_mes_sort, hc_inline_mes)
+            hc_mes.append(self.Structs.nx_hypercube_measure(hc_mes_sort, hc_inline_mes))
         nx_page = self.Structs.nx_page(0, 0, rows_to_return, no_of_columns)
-        hc_def = self.Structs.hypercube_def("$", [hc_dim], [hc_mes], [nx_page])
+        hc_def = self.Structs.hypercube_def("$", hc_dim, hc_mes, [nx_page])
         hc_response = self.eaa.create_object(self.app_handle, "CH01", "Chart", "qHyperCubeDef", hc_def)
         hc_handle = self.ega.get_handle(hc_response)
         self.egoa.get_layout(hc_handle)
         hc_data = self.egoa.get_hypercube_data(hc_handle, "/qHyperCubeDef", [nx_page])
-        elems = hc_data["qDataPages"][0]['qMatrix']
-        dim_list = []
-        mes_list = []
-        for elem in range(len(elems)):
-            dim_list.append(elems[elem][0]["qText"])
-            mes_list.append(elems[elem][1]["qNum"])
-        return dim_list, mes_list
+        no_of_columns = len(list_of_dimensions)+len(list_of_measures)
+        return hc_data, no_of_columns
+
+    @staticmethod
+    def convert_hypercube_to_matrix(hc_data, no_of_columns):
+        rows = hc_data["qDataPages"][0]['qMatrix']
+        matrix = [[0 for x in range(no_of_columns)] for y in range(len(rows))]
+        for col_idx, row in enumerate(rows):
+            for cell_idx, cell_val in enumerate(row):
+                matrix[col_idx][cell_idx] = cell_val['qText']
+        return [list(i) for i in zip(*matrix)]
+
+    @staticmethod
+    def convert_hypercube_to_inline_table(hc_data, table_name, no_of_columns):
+        rows = hc_data["qDataPages"][0]['qMatrix']
+        script = str.format('{0}:{1}Load * Inline[{1}',table_name, '\r\n')
+        inline_rows = ''
+        header_row = ''
+        for col_idx in range(len(rows)):
+            header_row = header_row + str.format('Column{0}{1}', col_idx, ',')
+        header_row = header_row[:-1] + '\r\n'
+        for row in rows:
+            for cell_val in row:
+                inline_rows = inline_rows + cell_val['qText'] + ','
+            inline_rows = inline_rows[:-1] + '\r\n'
+        return script + header_row + inline_rows + '];'
 
     def select_in_dimension(self,dimension_name, list_of_values):
         lb_field = self.eaa.get_field(self.app_handle, dimension_name)
--- test/test_qix_engine.py
+++ test/test_qix_engine.py
@@ -18,7 +18,10 @@ class TestQixEngine(unittest.TestCase):
         select_result = self.qixe.select_in_dimension('Alpha', ['A', 'C', 'E'])
         self.assertTrue(select_result[1] == [1,2], "Failed to select values")
         self.assertTrue(select_result[0]['qReturn'], "Failed to select values")
-        print self.qixe.create_hypercube(['Alpha'], ['=Sum(Num)'])
+        hc = self.qixe.create_hypercube(['Dim1', 'Dim2'], ['=Sum(Expression1)', '=Sum(Expression2)', '=Sum(Expression3)'])
+        hc_cols = self.qixe.convert_hypercube_to_matrix(hc[0], hc[1])
+        self.assertTrue(len(hc_cols) == 5, 'Failed to return proper number of columns')
+        print self.qixe.convert_hypercube_to_inline_table(hc[0], 'MyTable', hc[1])
 
     def tearDown(self):
         self.assertTrue(self.qixe.delete_app(self.opened_app), 'Failed to delete app')
